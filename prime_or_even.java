d45cd6cad51940bd57631ab1d3e82f7d44c3dd22
AniketKadu12 <134839149+AniketKadu12@users.noreply.github.com>
2023-05-28 15:08:34 +0530

    Add files via upload

---
+++ prime_or_even.java
@@ -0,0 +1,275 @@
+/*// To print the address of array //
+************************************
+class program1
+{
+	public static void main(String[]args)
+	{
+		int []a={1,2,3};
+		System.out.println(a);
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+	}
+}*/
+
+/* //To print the address of array//
+*************************************
+class program2
+{
+	public static void main(String[]args)
+	{
+     int []a={1,2,3};
+	 System.out.println(a[0]);
+	 System.out.println(a[1]);
+	 System.out.println(a[2]);
+	 System.out.println(a[3]);
+	}
+}
+error : ArrayIndexOutOfBoundsException*/
+
+ /*// Enter the user Data //
+//*******************************
+import java.util.Scanner;
+class program3
+{
+	public static void main(String[]args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size");
+		int size=sc.nextInt();
+		int a[]=new int [size];
+		for (int i=0;i<a.length;i++)
+		{
+			System.out.println("Enter the element");
+			a[i]=sc.nextInt();
+		}
+		System.out.println("Show the element");
+		for (int i=0;i<a.length;i++)
+		{
+			System.out.print(a[i]);
+		}
+    }
+}*/
+
+/*// Enter the user data the space between to element //
+//****************************************************
+import java.util.Scanner;
+class program4
+{
+	public static void main(String[]args)
+	{
+		Scanner sc=new Scanner (System.in);
+		System.out.println("Enter the element");
+		int size=sc.nextInt();
+		int a[]=new int [size];
+		for (int i=0;i<a.length;i++)
+		{
+			System.out.println("Enter the element");
+			a[i]=sc.nextInt();
+		}
+		System.out.print("[");
+		for (int i=0;i<a.length;i++)
+		{
+			System.out.print(a[i]);
+			if (i<a.length-1)
+			{
+				System.out.print(" ");
+			}
+		}
+		System.out.print("]");
+	}
+
+}*/
+
+/*// Create the charater type data array store the element then print //
+//******************************************************************
+import java.util.Scanner;
+class program5
+{
+	public static void main(String[]args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size");
+		int size=sc.nextInt();
+		char c1[]=new char [size];
+		for (int i=0;i<c1.length;i++)
+		{
+			System.out.println("Enter the charater");
+			c1[i]=sc.next().charAt(0);
+		}
+		System.out.print("[");
+		for (int i=0;i<c1.length;i++)
+		{
+			System.out.print(c1[i]);
+			if (i<c1.length-1)
+			{
+				System.out.print(" ");
+			}
+		}
+		System.out.print("]");
+	}
+}*/
+
+/*// Create char type array store the element and print only vowels //
+//*****************************************************************
+import java.util.Scanner;
+class vowal
+{
+	public static void main(String []args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size");
+		int size=sc.nextInt();
+		char c1[]=new char [size];
+		for (int i=0;i<c1.length;i++)
+		{
+			System.out.println("Enter the charater");
+			c1[i]=sc.next().charAt(0);
+		}
+		System.out.print("[");
+		for (int i=0;i<c1.length;i++)
+		{
+			if (c1[i]=='a' || c1[i]=='e' || c1[i]=='i' || c1[i]=='o' || c1[i]=='u' ||
+				c1[i]=='A' || c1[i]=='E' || c1[i]=='I' || c1[i]=='O' || c1[i]=='U')
+			{
+			  if (i<c1.length-1)
+			  {
+				  System.out.print(c1[i]);
+			  }
+			  System.out.print(" ");
+			}
+		}
+        System.out.print("]");
+	}
+}*/
+
+/*// Create char type array store the element print accevaule of char//
+//*******************************************************************
+
+import java.util.Scanner;
+class acce_vaule
+{
+	public static void main(String []args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size");
+		int size=sc.nextInt();
+		char a[]=new char [size];
+		for (int i=0;i<a.length;i++)
+		{
+			System.out.println("Enter the element");
+			a[i]=sc.next().charAt(0);
+		}
+		System.out.println("Show the acce vaule");
+		for (int i=0;i<a.length;i++ )
+		{
+			int b=a[i];
+			{
+				System.out.println(b);
+			}
+		}
+	}
+}*/
+
+
+/*//Create the floate type array store the element and print the element in intger format//
+//**************************************************************************************
+
+import java.util.Scanner;
+class floate_type_array
+{
+	public static void main(String[]args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size");
+		int size=sc.nextInt();
+		float f1[]=new float [size];
+		
+		for (int i=0;i<f1.length;i++)
+		{
+			System.out.println("Enter the element");
+			f1[i]=sc.nextFloat();
+		}
+		System.out.println("Show all float element beforeconverting in int ");
+       for (int i=0;i<f1.length;i++)
+		{
+		
+			System.out.println(f1[i]);
+		}
+		System.out.println("Show all float element after converting in int ");
+		for (int i=0;i<f1.length;i++)
+		{
+		
+			System.out.println((int)f1[i]);
+		}
+	}
+}*/
+
+
+/*//Create inter type array store the element cheak the element odd or even if the element in even to print sqaure and the element is odd to print cube//
+//*****************************************************************************************************************************************************
+
+import java.util.Scanner;
+class prime_or_even
+{
+	public static void main(String[]args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size");
+		int size=sc.nextInt();
+		int a[]=new int [size];
+		for (int i=0;i<a.length;i++)
+		{
+			System.out.println("Enter the element");
+			a[i]=sc.nextInt();
+		}
+		for (int i=0;i<a.length;i++)
+		{
+			if (a[i]%2==0)
+			{
+				System.out.println("sqaure of no");
+				System.out.println(a[i]*a[i]);
+			}
+			else 
+			{
+				System.out.println("cube of no");
+				System.out.println(a[i]*a[i]*a[i]);
+			}
+		}
+	}
+}/*
+
+//wajp to print the addition (sum) of element in given array//
+//*************************************************//
+
+import java.util.Scanner;
+class add_of_array
+{
+	public static void main(String []args);
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size");
+		int size=sc.nextInt();
+		int b[]=new int [size];
+		for (int i=0;i<b.length;i++)
+		{
+			System.out.println("Enter the element");
+			b[i]=sc.nextInt();
+		}
+		System.out.println("Show the add of element");
+		for (int i=0;i<b.length;i++)
+		{
+			sum=sum+b[i]
+		}
+			System.out.println(sum);
+	}
+}
+
+
+
+
+
+
+
+
+
---
+++ program5.java
@@ -0,0 +1,145 @@
+/*// To print the address of array //
+************************************
+class program1
+{
+	public static void main(String[]args)
+	{
+		int []a={1,2,3};
+		System.out.println(a);
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+	}
+}*/
+
+/* //To print the address of array//
+*************************************
+class program2
+{
+	public static void main(String[]args)
+	{
+     int []a={1,2,3};
+	 System.out.println(a[0]);
+	 System.out.println(a[1]);
+	 System.out.println(a[2]);
+	 System.out.println(a[3]);
+	}
+}
+error : ArrayIndexOutOfBoundsException*/
+
+ /*// Enter the user Data //
+//*******************************
+import java.util.Scanner;
+class program3
+{
+	public static void main(String[]args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size");
+		int size=sc.nextInt();
+		int a[]=new int [size];
+		for (int i=0;i<a.length;i++)
+		{
+			System.out.println("Enter the element");
+			a[i]=sc.nextInt();
+		}
+		System.out.println("Show the element");
+		for (int i=0;i<a.length;i++)
+		{
+			System.out.print(a[i]);
+		}
+    }
+}*/
+
+/*// Enter the user data the space between to element //
+//****************************************************
+import java.util.Scanner;
+class program4
+{
+	public static void main(String[]args)
+	{
+		Scanner sc=new Scanner (System.in);
+		System.out.println("Enter the element");
+		int size=sc.nextInt();
+		int a[]=new int [size];
+		for (int i=0;i<a.length;i++)
+		{
+			System.out.println("Enter the element");
+			a[i]=sc.nextInt();
+		}
+		System.out.print("[");
+		for (int i=0;i<a.length;i++)
+		{
+			System.out.print(a[i]);
+			if (i<a.length-1)
+			{
+				System.out.print(" ");
+			}
+		}
+		System.out.print("]");
+	}
+
+}*/
+
+/*// Create the charater type data array store the element then print //
+//******************************************************************
+import java.util.Scanner;
+class program5
+{
+	public static void main(String[]args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size");
+		int size=sc.nextInt();
+		char c1[]=new char [size];
+		for (int i=0;i<c1.length;i++)
+		{
+			System.out.println("Enter the charater");
+			c1[i]=sc.next().charAt(0);
+		}
+		System.out.print("[");
+		for (int i=0;i<c1.length;i++)
+		{
+			System.out.print(c1[i]);
+			if (i<c1.length-1)
+			{
+				System.out.print(" ");
+			}
+		}
+		System.out.print("]");
+	}
+}*/
+
+// Create char type array store the element and print only vowels //
+//*****************************************************************
+import java.util.Scanner;
+class program6
+{
+	public static void main(String []args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size");
+		int size=sc.nextInt();
+		char c1=new char [size];
+		for (int i=0;i<c1.length;i++)
+		{
+			System.out.println("Enter the charater");
+			c1[i]=sc.nextInt();
+		}
+		System.out.println("[")
+		for (int i=0;i<c1.length;i++)
+		{
+			if (c1[i]='a' || c1[i]='e' || c1[i]='i' || c1[i]='o' || c1[i]='u' ||
+				c1[i]='A' || c1[i]='E' || c1[i]='I' || c1[i]='O' || c1[i]='U')
+			{
+			  if (i<c1.length-1)
+			  {
+				  System.out.println(" ");
+			  }
+			}
+		}
+        System.out.println("]")
+	}
+}
+
+
